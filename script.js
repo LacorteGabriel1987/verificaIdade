65764180584a8dd2646ecf0141b0914c407f5823
Gabriel Lacorte <gabrielslacorte@hotmail.com>
2022-08-17 16:15:48 -0300

    Criando js e ajustando html

---
+++ script.js
@@ -0,0 +1,10 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYears()
+    var fano = document.getElementById("textano")
+    var res = documento.getElementById("res")
+}
+
+
+
+
