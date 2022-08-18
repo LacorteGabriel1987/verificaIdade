94c276bd094cbd6e3e0afaad6987d88363ddfffd
Gabriel Lacorte <gabrielslacorte@hotmail.com>
2022-08-18 14:26:32 -0300

    Ajustando js

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
-function verificar(){
+function verificar() {
     var data = new Date()
-    var ano = data.getFullYears()
+    var ano = data.getFullYear()
     var fano = document.getElementById("textano")
-    var res = documento.getElementById("res")
+    var res = document.getElementById("res")
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert("[ERRO] Verifique os dados e tente novamente!")
+    } else {
+    var fsex = document.getElementsByName("radsex")
+    var idade = ano - Number(fano.value)
+    var gênero = ''
+    var img = document.createElement("img")
+    img.setAttribute("id", "foto")
+    if (fsex[0].checked) {
+        gênero = "Homem"
+        if (idade >= 0 && idade < 10) {
+            //criança
+            img.setAttribute(`src`, 'img/bebe-h.jpg')
+        } else if (idade < 21) {
+            //jovem
+            img.setAttribute(`src`, 'img/')
+        } else if (idade < 50) {
+            //Adulto
+            img.setAttribute(`src`, 'img/')
+        } else {
+            //Idoso
+            img.setAttribute(`src`, 'img')
+        }
+    } else if (fsex[1].checked) {
+        gênero = "Mulher"
+        if (idade >= 0 && idade < 10) {
+            //criança
+            img.setAttribute(`src`, 'img/bebe-h.jpg')
+        } else if (idade < 21) {
+            //Jovem
+            img.setAttribute(`src`, 'img/bebe-h.jpg')
+        } else if (idade < 50) {
+            //Adulto
+            img.setAttribute(`src`, 'img/bebe-h.jpg')
+        } else
+            //idoso
+            img.setAttribute(`src`, 'img/bebe-h.jpg')
+    }
 }
+            res.style.textAlign = 'center'
+            res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+            res.appendChild(img)
 
+        }
 
 
 
